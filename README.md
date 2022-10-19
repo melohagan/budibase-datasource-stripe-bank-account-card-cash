22fdaed4bd3aa909ed3c5f8c184ff741ba349ed3
Mel O'Hagan <mel@budibase.com>
2022-10-19 14:35:17 +0100

    init

---
+++ README.md
@@ -0,0 +1,20 @@
+# Stripe Bank account and Card datasource
+
+Official Stripe docs here: https://stripe.com/docs/api/customer_bank_accounts?lang=node
+
+# Description
+Manage Stripe bank accounts and cards
+
+Find out more about [Budibase](https://github.com/Budibase/budibase).
+
+## Instructions
+
+To build your new  plugin run the following in your Budibase CLI:
+```
+budi plugins --build
+```
+
+You can also re-build everytime you make a change to your plugin with the command:
+```
+budi plugins --watch
+```
---
+++ src/datasource.ts
@@ -0,0 +1,43 @@
+import { IntegrationBase } from "@budibase/types"
+import Stripe from "stripe"
+
+class CustomIntegration implements IntegrationBase {
+  private readonly stripe: Stripe
+
+  constructor(config: { apiKey: string; }) {
+    this.stripe = new Stripe(config.apiKey, {
+      apiVersion: '2022-08-01'
+    })
+  }
+
+  async create(query: { customer: string; source: string; }) {
+    return await this.stripe.customers.createSource(query.customer, { source: query.source })
+  }
+
+  async read(query: { customer: string; id: string;  }) {
+    return await this.stripe.customers.retrieveSource(query.customer, query.id)
+  }
+
+  async update(query: { customer: string; id: string; body: string}) {
+    return await this.stripe.customers.updateSource(query.customer, query.id, JSON.parse(query.body))
+  }
+
+  async list(query: { customer: string; limit: number; endingBefore: string; startingAfter: string; object: string; }) {
+    return await this.stripe.customers.listSources(query.customer, {
+      object: query.object,
+      ending_before: query.endingBefore,
+      starting_after: query.startingAfter,
+      limit: query.limit
+    })
+  }
+
+  async delete(query: { customer: string; id: string; }) {
+    return await this.stripe.customers.deleteSource(query.customer, query.id)
+  }
+
+  async verify(query: { customer: string; id: string; }) {
+    return await this.stripe.customers.verifySource(query.customer, query.id)
+  }
+}
+
+export default CustomIntegration
